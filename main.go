3be022d9794a0f2a8c13e2e76d145b6d5a802c79
Raffaele Di Fazio <raffaele.di.fazio@zalando.de>
2017-04-17 18:50:49 +0200

    Added instrumentation

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 	"os"
 
 	"github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	httptransport "github.com/go-kit/kit/transport/http"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -25,6 +28,30 @@ func main() {
 	// middlewares
 	svc = loggingMiddleware(logger)(svc)
 
+	// TODO understand, is this really the right place to plug the middlewares?
+	// adding instrumentation
+	fieldKeys := []string{"method", "error"}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "group",
+		Subsystem: "echo_service",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys)
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "group",
+		Subsystem: "echo_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKeys)
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "group",
+		Subsystem: "echo_service",
+		Name:      "count_result",
+		Help:      "The result of each count method.",
+	}, []string{})
+
+	svc = instrumentingMiddleware(requestCount, requestLatency, countResult)(svc)
+
 	echoHandler := httptransport.NewServer(
 		makeEchoEndpoint(svc),
 		decodeEchoRequest,
@@ -32,6 +59,8 @@ func main() {
 		httptransport.ServerErrorLogger(logger),
 	)
 	http.Handle("/echo", echoHandler)
+	http.Handle("/metrics", promhttp.Handler())
+
 	logger.Log("msg", "HTTP server started", "addr", *listen)
 	logger.Log("err", http.ListenAndServe(*listen, nil))
 }
---
+++ metrics.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"time"
+
+	"github.com/go-kit/kit/metrics"
+)
+
+func instrumentingMiddleware(
+	requestCount metrics.Counter,
+	requestLatency metrics.Histogram,
+	countResult metrics.Histogram,
+) ServiceMiddleware {
+	return func(next EchoService) EchoService {
+		return instrmw{requestCount, requestLatency, countResult, next}
+	}
+}
+
+type instrmw struct {
+	requestCount   metrics.Counter
+	requestLatency metrics.Histogram
+	countResult    metrics.Histogram
+	EchoService
+}
+
+func (mw instrmw) Echo(s string) (output string) {
+	defer func(begin time.Time) {
+		lvs := []string{"method", "echo", "error"}
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	output = mw.EchoService.Echo(s)
+	return
+}
